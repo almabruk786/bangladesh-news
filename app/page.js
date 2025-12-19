5e66f3795cbbe1036b121506e0b813cc2c14c8ab
Md Arif Mainuddin <almabruk786@gmail.com>
2025-12-20 01:53:27 +0600

    Fix: Remove hardcoded bg-white from Home page to fix Dark Mode contrast

--- app/page.js
+++ app/page.js
@@ -72,7 +72,7 @@ export default function Home() {
   );
 
   return (
-    <div className="min-h-screen bg-white">
+    <div className="min-h-screen">
       <script
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(generateItemListSchema(data.realLatestNews)) }}
